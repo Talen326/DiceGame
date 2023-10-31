59b5df4d375f4f8e027f7b9668bedf30e0fd97ae
Computer Science <computerscience@Locals-MacBook-Air.local>
2023-10-31 09:03:29 -1000

    Added Languages

--- DiceGame/src/DiceGame.java
+++ DiceGame/src/DiceGame.java
@@ -2,43 +2,64 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class DiceGame {
-
+	private static String[][] Language;
+	private static int lang=0;
+	
 	public static void main(String[] args) {
 		int die1;
 		int die2;
 		String[] playerChoices;
+		Language = initLang(); 
+		String langChoice = getInput(Language[lang][10]);
+		lang = Integer.parseInt(langChoice);
 		die1 = roll();
 		die2 = roll();
 		String choice;
-		choice = getInput("Do you want to play?\nYes\nNo");
+		choice = getInput(Language[lang][0]);
 		choice = choice.toLowerCase();
 		int count = 0;
-		String playerCount = getInput("How many players are there?");
+		String playerCount = getInput(Language[lang][1]);
 		count = Integer.parseInt(playerCount);
 		playerChoices = new String[count];
-		while(choice.equals("yes")) {
+		while(choice.equals(Language[0][2])) {
 			
 			for(int c= 0; c < count; c++) {
-			playerChoices[c] = getInput("Player "+(c+1)+": [0] Even\n[1] Odd");
+			playerChoices[c] = getInput(Language[lang][3]+(c+1)+Language[lang][4]);
 			
 			}
-			System.out.println("Die 1: "+die1+"\nDie 2: "+die2);
+			System.out.println(Language[lang][5]+die1+Language[lang][6]+die2);
 			//Start a new loop here.
 			for(int c=0; c<count;c++) {
 				if(didIWin(die1,die2,playerChoices[c]))
 				{
-					System.out.println("Player "+(c+1)+" wins!");
+					System.out.println(Language[lang][3]+(c+1)+Language[lang][7]);
 				}else
 				{
-					System.out.println("Player "+(c+1)+" loses!");
+					System.out.println(Language[lang][3]+(c+1)+Language[lang][8]);
 				}
 			}
 			
-			choice = getInput("Do you want to play?\nYes\nNo");
+			choice = getInput(Language[0][9]);
 			choice = choice.toLowerCase();
 			die1 = roll();
 			die2 = roll();
 		}
+	}
+	private static String[][] initLang(){
+		String[][] temp = new String[][] {
+			{"Do you want to play?\nYes\nNo","How many players are there?","yes",
+				"Player ",": [0] Even\n[1] Odd","Die 1: ",
+				"\nDie 2: "," wins!"," loses!",
+				"Do you want to play again?\nYes\nNo","Enter language: \n[0] English\n[1] Hawaiian Pidgen"},
+			{
+					"Like try?\nyeesah\nnaaah","How many dakine Braddahs","yessah",
+					"Bradda ", ": [0] Even\n[1] Odd","Die 1",
+					"\nDie 2: ","Winnah!", "No need win.",
+					"Like try again?\nYessah\nNo"
+					
+			}
+		};
+			return temp;
 	}
 	private static boolean didIWin(int die1, int die2, String choice)
 	{
