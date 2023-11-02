6775a716c26fee7f8ed8031c04946a4a2b4c79b0
Computer Science <computerscience@Locals-MacBook-Air.local>
2023-11-02 08:32:33 -1000

    added japanese translation

--- DiceGame/src/DiceGame.java
+++ DiceGame/src/DiceGame.java
@@ -2,69 +2,75 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class DiceGame {
-	private static String[][] Language;
-	private static int lang=0;
+
+	private static String[][] language;
+	private static int lang = 0;
 	
 	public static void main(String[] args) {
 		int die1;
 		int die2;
 		String[] playerChoices;
-		Language = initLang(); 
-		String langChoice = getInput(Language[lang][10]);
+		language = initLang();
+		String langChoice = getInput(language[lang][10]);
 		lang = Integer.parseInt(langChoice);
 		die1 = roll();
 		die2 = roll();
 		String choice;
-		choice = getInput(Language[lang][0]);
+		choice = getInput(language[lang][0]);
 		choice = choice.toLowerCase();
 		int count = 0;
-		String playerCount = getInput(Language[lang][1]);
+		String playerCount = getInput(language[lang][1]);
 		count = Integer.parseInt(playerCount);
 		playerChoices = new String[count];
-		while(choice.equals(Language[0][2])) {
+		while(choice.equals(language[lang][2])) {
 			
 			for(int c= 0; c < count; c++) {
-			playerChoices[c] = getInput(Language[lang][3]+(c+1)+Language[lang][4]);
+			playerChoices[c] = getInput(language[lang][3]+(c+1)+language[lang][4]);
 			
 			}
-			System.out.println(Language[lang][5]+die1+Language[lang][6]+die2);
+			System.out.println(language[lang][5]+die1+language[lang][6]+die2);
 			//Start a new loop here.
 			for(int c=0; c<count;c++) {
 				if(didIWin(die1,die2,playerChoices[c]))
 				{
-					System.out.println(Language[lang][3]+(c+1)+Language[lang][7]);
+					System.out.println(language[lang][3]+(c+1)+language[lang][7]);
 				}else
 				{
-					System.out.println(Language[lang][3]+(c+1)+Language[lang][8]);
+					System.out.println(language[lang][3]+(c+1)+language[lang][8]);
 				}
 			}
 			
-			choice = getInput(Language[0][9]);
+			choice = getInput(language[lang][9]);
 			choice = choice.toLowerCase();
 			die1 = roll();
 			die2 = roll();
 		}
 	}
-	private static String[][] initLang(){
+	private static String[][] initLang() {
 		String[][] temp = new String[][] {
-			{"Do you want to play?\nYes\nNo","How many players are there?","yes",
-				"Player ",": [0] Even\n[1] Odd","Die 1: ",
-				"\nDie 2: "," wins!"," loses!",
-				"Do you want to play again?\nYes\nNo","Enter language: \n[0] English\n[1] Hawaiian Pidgen"},
-			{
-					"Like try?\nyeesah\nnaaah","How many dakine Braddahs","yessah",
-					"Bradda ", ": [0] Even\n[1] Odd","Die 1",
-					"\nDie 2: ","Winnah!", "No need win.",
-					"Like try again?\nYessah\nNo"
-					
-			}
+			{"Do you want to play?\nYes\nNo", "How many players are there?", "yes", 
+				"Player ", ": [0] Even\n[1] Odd", "Die 1: ",
+				"\nDie 2: ", " wins!", " loses!", "Do you want to play?\nYes\nNo", "Enter language:\n[0] English\n[1] Hawaiian Pidgen\n[2] Japanese"},
+			{"Like try?\nYessah\nNo", "How many dakine Braddahs?", "yessah",
+			"Braddah ", ": [0] Even\n[1] Odd", "Die 1: ",
+			"\nDie 2: ", " winnah!", " no need win.","Like try again?\nYessah\nNo"},
+			
+				{
+				"プレイしますか？\nはい\nいいえ", "何人遊びますか？", "はい", 
+				"プレイヤー ", ": \n[0] 偶数\n[1] 奇数", "サイコロ 1: ",
+				"\nサイコロ 2: ", " 勝ち!", " 負け!", "もう一回遊びますか?\nはい\nいいえ"}
 		};
-			return temp;
+		return temp;
 	}
 	private static boolean didIWin(int die1, int die2, String choice)
 	{
 		
-		return false;
+		if(((die1+die2)%2 == 0 && Integer.parseInt(choice) == 0) || ((die1+die2)%2 == 1 && Integer.parseInt(choice) == 1)) {
+			return true;
+		} else {
+			return false;
+		}
+		
 	}
 	
 	
